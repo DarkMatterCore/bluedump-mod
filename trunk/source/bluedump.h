1bf9e191faaea2a4ce7ae8018a60c14657ed1bb0
pabloacurielz@gmail.com <pabloacurielz@gmail.com@3c81372a-c99d-befd-42a9-2f97fd2a9d8e>
2013-09-07 09:38:35 +0000

    MOD v0.6 (September 7th, 2013 - DarkMatterCore and JoostinOnline):
    * Added compatibility with content.bin files stored in SD:/private/wii/title. You can switch between normal WAD dumping and content.bin conversion modes with +/R.
    * Added code to read the OTP contents and copy them to a previously aligned memory block. This is needed to access both the PRNG Key and the Console ID, which are used in the content.bin conversion process.
    * Added compatibility with Classic Controllers (thanks to JoostinOnline!).
    * Optimized name reading functions. Instead of allocating a memory block and then returning it (which is unsafe), now a single global character string is used, with its current value being updated by using snprintf() (thanks to JoostinOnline!).
    * Added meta.xml argument support to enable or disable the logfile creation without having to recompile the entire application (thanks to JoostinOnline!).
    * New sexy, smexy icon (thanks to JoostinOnline!).
    * Some other changes and simplifications that reduce the overall CPU use (thanks to JoostinOnline!).

---
+++ trunk/source/bluedump.h
@@ -0,0 +1,83 @@
+#ifndef __BLUEDUMP_H__
+#define __BLUEDUMP_H__
+
+#define BLOCKSIZE		0x4000 // 16 KB
+#define SD_BLOCKSIZE	0x8000 // 32 KB
+
+#define DIRENT_T_FILE 0
+#define DIRENT_T_DIR 1
+
+#define ROOT_DIR "/title"
+#define SD_ROOT_DIR "sd:/private/wii/title"
+#define DEVICE(x) (((x) == 0) ? (isSD ? "sd" : "usb") : (isSD ? "SD" : "USB"))
+
+#define TYPE_SAVEDATA 	0
+#define TYPE_TITLE 		1
+#define TYPE_IOS		2
+#define TYPE_SYSTITLE	3
+#define TYPE_GAMECHAN	4
+#define TYPE_DLC		5
+#define TYPE_HIDDEN		6
+#define TYPE_OTHER 		7
+
+#define TITLE_UPPER(x)		((u32)((x) >> 32))
+#define TITLE_LOWER(x)		((u32)(x))
+#define TITLE_ID(x,y)		(((u64)(x) << 32) | (y))
+
+#define round_up(x,n)   (-(-(x) & -(n)))
+#define round64(x)      round_up((x),0x40)
+#define round16(x)		round_up((x),0x10)
+
+#define MAX_CHARACTERS(x) ((sizeof((x))) / (sizeof((x)[0]))) // Returns the number of elements in an array
+
+#define ARROW " \x10"
+
+typedef struct _dirent
+{
+	char name[ISFS_MAXPATH + 1];
+	u16 version;
+	int type;
+	int function;
+	u32 ownerID;
+	u16 groupID;
+	u8 attributes;
+	u8 ownerperm;
+	u8 groupperm;
+	u8 otherperm;
+} dirent_t;	
+
+/* 'WAD Header' structure */
+typedef struct 
+{
+	/* Header length */
+	u32 header_len;
+
+	/* WAD type */
+	u16 type;
+
+	u16 padding;
+
+	/* Data length */
+	u32 certs_len;
+	u32 crl_len;
+	u32 tik_len;
+	u32 tmd_len;
+	u32 data_len;
+	u32 footer_len;
+} ATTRIBUTE_PACKED wadHeader;
+
+wadHeader *header;
+
+typedef struct map_entry
+{
+	char filename[8];
+	u8 sha1[20];
+} __attribute__((packed)) map_entry_t;
+
+map_entry_t *cm;
+size_t content_map_size;
+size_t content_map_items;
+
+void bluedump_loop(void);
+
+#endif
--- trunk/source/main.c
+++ trunk/source/main.c
@@ -4,17 +4,20 @@
 #include <gccore.h>
 #include <ogcsys.h>
 
+#include "bluedump.h"
 #include "tools.h"
 
-extern void __exception_setreload(int);
+// Check if string X is in current argument
+#define CHECK_ARG(X) (!strncmp((X), argv[i], sizeof((X))-1))
+#define CHECK_ARG_VAL(X) (argv[i] + sizeof((X))-1)
 
-void bluedump_loop(void);
+extern void __exception_setreload(int);
 
 int main(int argc, char* argv[])
 {
 	__exception_setreload(10);
 	
-	int ret;
+	int i, ret;
 	
 	Init_Console();
 	printf("\x1b[%u;%um", 37, false);
@@ -24,6 +27,26 @@ int main(int argc, char* argv[])
 	WPAD_SetDataFormat(WPAD_CHAN_0, WPAD_FMT_BTNS_ACC_IR);
 	
 	printheadline();
+	
+	__debug = false;
+	for (i = 1; i < argc; i++)
+	{
+		if (CHECK_ARG("debug="))
+		{
+			if (atoi(CHECK_ARG_VAL("debug=")) == 1)
+			{
+				__debug = true;
+				printf("Debug mode activated!\n\n");
+				printf("Overall application performance may be slower than before.\n");
+				printf("This is completely normal, and is because debug info will be constantly\n");
+				printf("written to the \"BlueDump.log\" file in the selected storage device.\n\n");
+				printf("Press any button to continue...");
+				DetectInput(DI_BUTTONS_DOWN);
+				resetscreen();
+				printheadline();
+			}
+		}
+	}
 	
 	ret = ahbprot_menu();
 	if (ret < 0)
